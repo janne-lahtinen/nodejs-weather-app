282a8338afec89605f125290be441983f6aedabc
janne-lahtinen <janne.lahtinen@grafinote.fi>
2023-01-23 09:44:09 +0200

    remove comment

--- public/js/app.js
+++ public/js/app.js
@@ -22,7 +22,7 @@ weatherForm.addEventListener('submit', (event) => {
     } else {
       messageOne.classList.add('bold')
       messageOne.textContent = data.location 
-      messageTwo.textContent = `${data.forecast.desc}. It is currently ${data.forecast.temp} degrees out and it feels like ${data.forecast.feel} degrees out.`//data.forecast
+      messageTwo.textContent = `${data.forecast.desc}. It is currently ${data.forecast.temp} degrees out and it feels like ${data.forecast.feel} degrees out.`
       const img = document.createElement('img')
       img.src = data.forecast.icon
       img.alt = 'Weather icon'
